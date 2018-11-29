e26eddfa55a37c88b27ee167ef61b873e4aa6247
Jake Thorson <37002220+jac0basaurus@users.noreply.github.com>
2018-11-28 17:20:11 -0700

    Update timeseriesPlot.m
    Added ability to plot field data as well as calibration data

--- AddtlFunctions/PlottingFunctions/timeseriesPlot.m
+++ AddtlFunctions/PlottingFunctions/timeseriesPlot.m
@@ -1,46 +1,130 @@
-function timeseriesPlot(t,~,Y,Y_hat,valList,settingsSet)
+function timeseriesPlot(t,X,Y,Y_hat,valList,~,settingsSet)
 %Plots the validation and calibration estimates for each fold
 
 %Get names of current pod, regression, and validation methods
-podName = settingsSet.currentPod;
-valName = settingsSet.currentValidation;
-regName =   settingsSet.currentRegression;
+podName = settingsSet.podList.podName{settingsSet.loops.j};
+refName = Y.Properties.VariableNames{1};
 
-Y_hat_cal = Y_hat.cal; %Extract calibrated estimates
-Y_hat_val = Y_hat.val; %Extract validation estimates
+nRegs = length(settingsSet.modelList); %Number of regression functions
+nVal = length(settingsSet.valList); %Number of validation functions
+nFolds = settingsSet.nFolds;  %Number of folds to evaluate
+nReps = settingsSet.nFoldRep;  %Number of folds actually used
+nSets = numel(fieldnames(Y_hat)); %Number of estimate sets
+setList = {'Train','Test','Field'};
+
+%% Get reference values together
 Y = table2array(Y); %Converts to array for use in plotting
-t = datenum(t); %Convert to datenums for plotting
 
-%First plot the reference values colored by validation set
-g(1,1) = gramm('x',t,'y',Y(:,1),'color',valList);% Create a gramm object
-g(1,1).geom_line('alpha',0.5);% Plot raw data as points
-g(1,1).set_names('x',' ','y','Reference','color','Val. Set'); % Set appropriate names for legends
-g(1,1).set_color_options('map','lch'); %Set color
-g(1,1).set_datetick('x'); %Make x-axis display dates/times instead of the datenum
-g(1,1).set_title('Reference Concentrations');% Set figure title
+%Add an elapsed time variable that doesn't include long gaps
+[X, ~] = makeDiscontTElaps(X, t, settingsSet);
+t = X.telapsed;
+y_plot = cell(nVal,nFolds); %Dimensions: (i=cal/val, k=nVal, kk=nFolds)
+tref_plot = cell(nVal,nFolds);
 
-%The plot the estimates on data used for training the model
-g(1,2) = gramm('x',Y_hat_cal(:,1),'y',Y_hat_cal(:,2),'color',Y_hat_cal(:,3));% Create a gramm object
-g(1,2).geom_line('alpha',0.5);% Plot raw data as points
-g(1,2).set_names('x',' ','y','Estimate','color','Fold #'); % Set appropriate names for legends
-g(1,2).set_color_options('map','lch'); %Set color
-g(1,2).set_datetick('x');
-g(1,2).set_title('Calibrated Timeseries');% Set figure title
+%Grouping variables
+G.reffold = ones(nVal,nFolds)*(nReps+1);
+G.refval = strings(nVal,nFolds);
+G.refcalval = strings(nVal,nFolds);
 
-%The plot the estimates on data used for validating the model
-g(1,3) = gramm('x',Y_hat_val(:,1),'y',Y_hat_val(:,2),'color',Y_hat_val(:,3));% Create a gramm object
-g(1,3).geom_line('alpha',0.5);% Plot raw data as points
-g(1,3).set_names('x',' ','y','Estimate','color','Fold #'); % Set appropriate names for legends
-g(1,3).set_color_options('map','lch'); %Set color
-g(1,3).set_datetick('x');
-g(1,3).set_title('Validation Timeseries');% Set figure title
-
-%Set the overall title
-g.set_title(['Pod: ' podName ', Validation: ' valName ', Regression: ' regName]);
-
-% Do the actual drawing
-figure('Position',[100 100 1000 400]);
-g.draw();
-
+%% Assign values for reference plot
+for m=1:nRegs
+    for k=1:nVal
+        %Name of validation
+        G.refval(k,:) = settingsSet.valList{k};
+        for kk=1:nFolds
+            %Calibration and validation timeseries for reference cell array
+            G.reffold(:,kk) = kk;
+            y_plot{k,kk} = Y(valList{k}==kk,1);
+            tref_plot{k,kk} = t(valList{k}==kk);
+        end
+    end
 end
 
+%Make into categorical values for plotting
+G.refval = categorical(G.refval);
+G.refcalval = categorical(G.refcalval);
+
+%% Get estimated values together
+tempycal = Y_hat.cal;
+tempyval = Y_hat.val;
+
+if nSets == 3
+    tempyfield = Y_hat.field;
+    Y_hat = cell(nSets,nRegs,nVal,nReps);
+    Y_hat(3,:,:,:) = tempyfield; %Extract field estimates if they've been generated
+else
+    Y_hat = cell(nSets,nRegs,nVal,nReps);%Dimensions: (i=cal/val, m=nRegs, k=nVal, kk=nFolds)
+end
+Y_hat(1,:,:,:) = tempycal; %Extract calibrated estimates 
+Y_hat(2,:,:,:) = tempyval; %Extract validation estimates
+
+%Have to make cell matrix to hold timeseries for each plot
+t_plot = cell(nSets,nRegs,nVal,nReps); 
+
+%Grouping variables
+H.regs = strings(nSets,nRegs,nVal,nReps);
+H.vals = strings(nSets,nRegs,nVal,nReps);
+H.folds = ones(nSets,nRegs,nVal,nReps)*(nReps+1);
+H.calval = strings(nSets,nRegs,nVal,nReps);
+
+%% Assign values for estimate plot
+for zz = 1:nSets
+    H.calval(zz,:,:,:) = setList{zz};
+end
+
+for m=1:nRegs
+    %Name of regression
+    H.regs(:,m,:,:)=settingsSet.modelList{m};
+    for k=1:nVal
+        %Name of validation
+        H.vals(:,:,k,:) = settingsSet.valList{k};
+        for kk=1:nReps
+            %Grouping Variable and times for estimate cell array
+            H.folds(:,:,:,kk)=kk;
+            t_plot{1,m,k,kk} = t(valList{k}~=kk & valList{k}~=-1);
+            t_plot{2,m,k,kk} = t(valList{k}==kk);
+            if nSets==3
+                t_plot{3,m,k,kk} = t(valList{k}==-1);
+            end
+        end
+    end
+end
+H.regs = categorical(H.regs);
+H.vals = categorical(H.vals);
+H.calval = categorical(H.calval);
+
+
+%% Do plotting
+%% Reference Values
+g(1,1) = gramm('x',tref_plot,'y',y_plot,'color',G.reffold);% Create a gramm object
+g(1,1).facet_grid([],G.refval);
+g(1,1).geom_point();% Plot raw data as points
+g(1,1).set_names('x',' ', 'y','Reference', 'column','Validation', 'color','Fold'); % Set appropriate names for legends
+g(1,1).set_point_options('base_size',3,'step_size',3,'markers',{'o' 'p' 'd' '^' 'v' '>' '<' 's' 'h' '*' '+' 'x'});
+%g(1,1).set_color_options('map','matlab');
+%g(1,1).set_datetick('x'); %Make x-axis display dates/times instead of the datenum
+g(1,1).axe_property('TickDir','out','XGrid','on','Ygrid','on','GridColor',[0.5 0.5 0.5]);%,'xlim',[(min(t)-1) (max(t)+1)],'ylim',[(min(Y)*0.9) (max(Y)*1.1)]);
+%g(1,1).set_title('Reference Concentrations');% Set figure title
+g(1,1).set_layout_options('Position',[0 nRegs/(nRegs+2) 1.0 1-nRegs/(nRegs+2)]);
+g(1,1).set_text_options('base_size',10,'facet_scaling',1.2);
+
+%% Estimated Data
+g(2,1) = gramm('x',t_plot,'y',Y_hat,'color',H.folds,'lightness',H.calval);
+g(2,1).facet_grid(H.regs,H.vals,'column_labels',false,'scale','free_y');
+g(2,1).geom_point('alpha',1);
+g(2,1).set_point_options('base_size',3,'step_size',3,'markers',{'o' 'p' 'd' '^' 'v' '>' '<' 's' 'h' '*' '+' 'x'});
+%g(2,1).set_datetick('x');
+g(2,1).set_names('row','M', 'x','Elapsed (days)', 'y','Estimate', 'column','Validation', 'color','Fold', 'Lightness','Set');
+g(2,1).axe_property('TickDir','out','XGrid','on','Ygrid','on','GridColor',[0.5 0.5 0.5]);%,'xlim',[(min(t)-1) (max(t)+1)],'ylim',[(min(Y)*0.9) (max(Y)*1.1)]);
+%g(2,1).set_title('Estimated Concentrations');
+g(2,1).set_layout_options('Position',[0 0 1.0 nRegs/(nRegs+2)]);
+g(2,1).set_text_options('base_size',10,'facet_scaling',1.0);
+
+%% Draw the figure
+%figure('Position',[200 200 1000 800]);
+figure('Position',get( groot, 'Screensize' ));
+g.set_title(['Timeseries for Pod: ' podName ', Reference: ' refName]);
+g.draw();
+
+
+end
