2a857a5b42576c05894e5aa626a5d183c9abd9f2
Jake Thorson <37002220+jac0basaurus@users.noreply.github.com>
2018-11-28 17:26:04 -0700

    Update convertPodDatatoMat.m

--- AddtlFunctions/CoreFunctions/convertPodDatatoMat.m
+++ AddtlFunctions/CoreFunctions/convertPodDatatoMat.m
@@ -19,7 +19,7 @@ end% function
 function readFiles(filesStruct,podList)
 %{
 This sub function uses information from the Pod List to help convert raw
-Pod files into mat files for easier analysis
+Pod files into mat files for easier/faster analysis
 %}
 for i = 1:size(filesStruct.files.bytes,1)
     
@@ -44,45 +44,67 @@ for i = 1:size(filesStruct.files.bytes,1)
     matFilePath = char(matFilePath);
     filePath = char(filePath);
     
-    %% Convert the file if it has not already been converted (the .mat file doesn't exist)
-    if exist(matFilePath, 'file') ~= 2
-        fprintf('No mat file, reading plain text file for %s ...\n',char(fileName));
-        
-        %Get pod name for comparison with pod list
-        podName = [filesStruct.files.podType{i} filesStruct.files.podName{i}];
-        
-        %Find the entry in the pod list corresponding to the current pod
-        %file being imported
-        for j = 1:size(podList,1)
-            if all(podList.podName{j}==podName)
-                VarNames = podList.VarNames{j};
-                VarTypes = podList.VarTypes{j};
-                %VarTypes = strjoin(VarTypes,'');
-                numVars = length(VarNames);
+    %% Convert the file only if it has not already been converted (the .mat file doesn't exist)
+    if exist(matFilePath, 'file') == 2
+        continue
+    end%if statement that checks whether to make a new mat file
+    
+    fprintf('No mat file, reading plain text file for %s ...\n',char(fileName));
+    
+    %Get pod name for comparison with pod list
+    podName = [filesStruct.files.podType{i} filesStruct.files.podName{i}];
+    
+    %Find the entry in the pod list corresponding to the current pod
+    %file being imported
+    for j = 1:size(podList,1)
+        if all(podList.podName{j}==podName)
+            VarNames = podList.VarNames{j};
+            VarTypes = podList.VarTypes{j};
+            %VarTypes = strjoin(VarTypes,'');
+            numVars = length(VarNames);
+        end%if
+    end%loop of pods
+    assert(exist('VarNames','var')==1,['No inventory entry was located for this pod: ' podName]);
+    
+    %Get whether to try to read a header line for variable names
+    hasHeaders = filesStruct.files.hasHeaders(i);
+    delimiters = filesStruct.files.delimiter{i};
+    
+    %Set appropriate import options
+    opts = detectImportOptions(filePath,'NumHeaderLines',hasHeaders,...
+        'NumVariables',numVars, 'Delimiter',delimiters,'FileType','text');
+    opts.ImportErrorRule = 'omitrow';
+    opts.ConsecutiveDelimitersRule = 'split';
+    opts=setvartype(opts,VarTypes(1:length(opts.VariableNames)));
+    
+    %Read in the data file
+    rawData = readtable(filePath,opts);
+    
+    %Add columns if columns are missing
+    if length(opts.VariableNames)<length(VarNames)
+        for j = 1:(length(VarNames) - length(opts.VariableNames))
+            %Make a dummy column of zeros to fill missing data
+            tempMat = zeros(size(rawData,1),1);
+            
+            %Character columns are imported as cell arrays, so need to make the dummy column also cells for merting
+            if strcmpi(VarTypes{length(opts.VariableNames)+j},'char')
+                tempMat = num2cell(num2str(tempMat));
             end%if
-        end
-        assert(exist('VarNames','var')==1,['No inventory entry was located for this pod: ' podName]);
+            
+            %Make this a table (the default variable name will get fixed later)
+            tempMat = array2table(tempMat);
+        end%loop through missing columns
         
-        %Get whether to try to read a header line for variable names
-        hasHeaders = filesStruct.files.hasHeaders(i);
-        
-        %Create appropriate import options
-        opts = detectImportOptions(filePath,'NumHeaderLines',hasHeaders,...
-            'NumVariables',numVars, 'Delimiter',',','FileType','text');
-        opts.ImportErrorRule = 'omitrow';
-        opts.ConsecutiveDelimitersRule = 'split';
-        opts=setvartype(opts,VarTypes);
-        
-        %Read in the data
-        %rawData = readtable(filePath,'ReadVariableNames',logical(hasHeaders),'Format',VarTypes,'Delimiter',',');
-        rawData = readtable(filePath,opts);
-        
-        %Assign variable names from pod inventory sheet
-        rawData.Properties.VariableNames = VarNames;
-        
-        %Save the file and then clear the variable for next loop
-        save(matFilePath, 'rawData');
-        clear rawData;
-    end% if statement that checks whether to make a new mat file
-end% for look that goes through each file
-end
+        %Append the column to the end of the data
+        rawData = [rawData tempMat];
+    end%if statement for missing columns
+    
+    %Assign variable names from pod inventory sheet
+    rawData.Properties.VariableNames = VarNames;
+    
+    %Save the file and then clear the variable for next loop
+    save(matFilePath, 'rawData');
+    clear rawData;
+    
+end%for loop that goes through each file
+end%read files subfunction
